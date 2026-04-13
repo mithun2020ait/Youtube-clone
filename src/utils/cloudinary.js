b22e8f8e6e3efac18d1a08b1d8175be1b368bd6b
Suvendu Chakrabarty <suva123chak@gmail.com>
2026-04-13 17:51:22 +0600

    file upload with cloudinary and multer middleware setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        //upload the file on cloudinary
+        const uploadResponce = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file upload been  successfully
+        console.log("File uploaded successfully on cloudinary", uploadResponce.url);
+        return uploadResponce;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // delete the file from local storage
+        console.error("Error uploading file to Cloudinary:", error);
+        return null;
+    }
+}
+
+export default uploadOnCloudinary;
