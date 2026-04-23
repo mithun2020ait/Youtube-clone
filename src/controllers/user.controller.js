72732a6c9d6723490e195b27c6e41dffdf59013c
Suvendu Chakrabarty <suva123chak@gmail.com>
2026-04-23 08:21:25 +0600

    User registration started

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,34 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import { User } from '../models/user.model.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Ok"
-    })
+    const { fullName, email, username, password } = req.body;
+    console.log("email: ", email, "password: " , password);
+
+    if(
+        [fullName, email, username, password].some((fields) =>
+        fields?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const userExists = User.findOne({
+        $or: [{ email }, { username }]
+    }) 
+
+    if(userExists) {
+        throw new ApiError(400, "User already exists")
+    }
+
+    const avtarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avtarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    
 });
 
 export { registerUser };
