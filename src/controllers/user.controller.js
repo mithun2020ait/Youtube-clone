907b474e3e2a207efb5d6b85881e558c6ec9f7ab
Suvendu Chakrabarty <suva123chak@gmail.com>
2026-04-27 09:19:30 +0600

    add user controller and bug fixing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiError } from '../utils/ApiError.js';
 import { User } from '../models/user.model.js';
+import { uploadFileToCloudinary, uploadOnCloudinary } from '../utils/cloudinary.js';
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, username, password } = req.body;
@@ -28,7 +29,32 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Avatar is required")
     }
 
-    
+    const avatar = await uploadOnCloudinary(avtarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar) {
+        throw new ApiError(400, "Avatar upload failed")
+    }
+
+    const  user = awaitUser.create({
+        fullName,
+        email,
+        username: username.toLowerCase(),
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    })
+
+    const createUser = await User.findById(user._id).select("-password -refreshToken")
+
+    if(!createUser) {
+        throw new ApiError(500, "Some thing went wrong while creating user")
+    }
+
+    return res.status(201).json(
+        new ApiResponce(200, createUser, "User created successfully")
+    )
+
 });
 
 export { registerUser };
